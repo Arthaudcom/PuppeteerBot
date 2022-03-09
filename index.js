ede27ad709615db334fa6e6e36a33ca187f0ea23
Arthaudcom <arthaudmorin@gmail.com>
2022-03-09 17:25:33 -0500

    Stockage des mails dans un .txt avec fs #1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
     await page.goto('https://agencewebducanada.com/search?country=Quebec&ville=Qu%C3%A9bec&page=1');
 
     const Agences = 'a > div > img';
@@ -16,15 +16,22 @@ const puppeteer = require('puppeteer');
 
     const mailSelector = " div > div:nth-child(2) > a:nth-child(5)";
 
-    for (var pg = 1; pg < nbrPageListe; pg++) {
-        for (var pasAgence = 0; pasAgence < nbrSiteListe; pasAgence++) {  //tourne pour sélectionner chaque agence
+    for (var pg = 1; pg < nbrPageListe; pg++) { //selectionne chaque page
+        for (var pasAgence = 0; pasAgence < nbrSiteListe; pasAgence++) {  //selectionne chaque agence 
             await page.waitForSelector(Agences); //attend que le selecteur css se charge
             await clickSelector(Agences, page, pasAgence);
             try { //vérifie si la page existe bien
-                await page.waitForSelector(mailSelector, {timeout: 3000})
+                await page.waitForSelector(mailSelector, { timeout: 3000 })
                 if ((await (page.$eval(mailSelector, el => el.innerText))) != null) {  //récupère le texte dans le selecteur de mail si il est présent sur la page
                     const mail = await (page.$eval(mailSelector, el => el.innerText));
                     console.log(mail); //écrit le mail dans la console
+                    fs.writeFile('agences.txt', mail + "\n", { flag: 'a+' }, err => {
+                        if (err) {
+                          console.error(err)
+                          return
+                        }
+                        //file written successfully
+                      })
                 }
             } catch (error) {}
             await page.goBack();
