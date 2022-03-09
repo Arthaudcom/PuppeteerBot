337c007cea13ce9ae73c631ae9bc65b4d554701a
Arthaudcom <arthaudmorin@gmail.com>
2022-03-09 15:58:41 -0500

    detection automatique nbr agences et nbr pages #3

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
+    await page.goto('https://agencewebducanada.com/search?country=Quebec&ville=Qu%C3%A9bec&page=1');
+
     const Agences = 'a > div > img';
-    const nbrSiteList = 10;
-    const nbrPageListe = 30;
+    const nbrSiteListe = (await page.$$('#main-container > section > div > div:nth-child(2) > div')).length; //nombre d'agences par page
+    console.log(nbrSiteListe);
+    const element = await page.$('div > nav > ul > li:nth-child(12)');
+    const nbrPageListe = await page.evaluate(element => element.textContent, element);  //nombre de pages
+    console.log(nbrPageListe);
     const mailSelector = " div > div:nth-child(2) > a:nth-child(5)"
     const nameSelector = "#agency > h1"
 
-
-
-    await page.goto('https://agencewebducanada.com/search?country=Quebec&ville=Qu%C3%A9bec&page=5');
-
     for (var pg = 1; pg < nbrPageListe; pg++) {
-        for (var pas = 0; pas < nbrSiteList; pas++) {
+        for (var pas = 0; pas < nbrSiteListe; pas++) {
             await page.waitForSelector(Agences);
             await clickSelector(Agences, page, pas);
             await page.waitForSelector(mailSelector)
