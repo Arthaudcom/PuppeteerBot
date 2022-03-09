d286c12f15fd20fac6f61537209b84f0e62354de
Arthaudcom <arthaudmorin@gmail.com>
2022-03-09 16:32:04 -0500

    resolution du bug quand la page n'existe pas #2

--- index.js
+++ index.js
@@ -9,26 +9,30 @@ const puppeteer = require('puppeteer');
     const Agences = 'a > div > img';
     const nbrSiteListe = (await page.$$('#main-container > section > div > div:nth-child(2) > div')).length; //nombre d'agences par page
     console.log(nbrSiteListe);
+
     const element = await page.$('div > nav > ul > li:nth-child(12)');
     const nbrPageListe = await page.evaluate(element => element.textContent, element);  //nombre de pages
     console.log(nbrPageListe);
-    const mailSelector = " div > div:nth-child(2) > a:nth-child(5)"
-    const nameSelector = "#agency > h1"
+
+    const mailSelector = " div > div:nth-child(2) > a:nth-child(5)";
 
     for (var pg = 1; pg < nbrPageListe; pg++) {
-        for (var pas = 0; pas < nbrSiteListe; pas++) {
-            await page.waitForSelector(Agences);
-            await clickSelector(Agences, page, pas);
-            await page.waitForSelector(mailSelector)
-            if ((await (page.$eval(mailSelector, el => el.innerText)))!= null){
-            const mail = await (page.$eval(mailSelector, el => el.innerText));
-            console.log(mail);}
+        for (var pasAgence = 0; pasAgence < nbrSiteListe; pasAgence++) {  //tourne pour sélectionner chaque agence
+            await page.waitForSelector(Agences); //attend que le selecteur css se charge
+            await clickSelector(Agences, page, pasAgence);
+            try { //vérifie si la page existe bien
+                await page.waitForSelector(mailSelector, {timeout: 3000})
+                if ((await (page.$eval(mailSelector, el => el.innerText))) != null) {  //récupère le texte dans le selecteur de mail si il est présent sur la page
+                    const mail = await (page.$eval(mailSelector, el => el.innerText));
+                    console.log(mail); //écrit le mail dans la console
+                }
+            } catch (error) {}
             await page.goBack();
             await page.waitForSelector(Agences);
         }
 
-        await page.goto('https://agencewebducanada.com/search?country=Quebec&ville=Qu%C3%A9bec&page='+(pg+1));
-        
+        await page.goto('https://agencewebducanada.com/search?country=Quebec&ville=Qu%C3%A9bec&page=' + (pg + 1));
+
     }
     console.log('end');
 
