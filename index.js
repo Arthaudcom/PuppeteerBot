d6422fcde41d5c93825319ea875a30b73a69a0db
Arthaudcom <arthaudmorin@gmail.com>
2022-09-23 16:55:29 -0400

    Ajout d'une barre de chargement

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const cliProgress = require('cli-progress');
+const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+bar.etaBuffer = 12;
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    const url = 'https://agencewebducanada.com/search?country=Quebec';
+    const url = 'https://www.agenceswebduquebec.com/agences/liste/montreal';
 
     await page.goto(url);  //navigue vers l'url
 
-    const Agences = 'a > div > img';
-    const nbrSiteListe = (await page.$$('#main-container > section > div > div:nth-child(2) > div')).length; //nombre d'agences par page
-    console.log("nombre de site par page: "+nbrSiteListe);
+    const Agences = 'div.logo > a > img'; 
+    const nbrSiteListe = (await page.$$('div.logo > a > img')).length; //nombre d'agences par page
+    console.log("nombre de site par page: "+nbrSiteListe); 
+    const mailSelector = "#contact >p:nth-child(2) > a"; //selecteur css du mail
 
-    const element = await page.$('div > nav > ul > li:nth-child(12)');
-    const nbrPageListe = await page.evaluate(element => element.textContent, element);  //nombre de pages
-    const waitingTime = nbrPageListe*nbrSiteListe/80;
-    console.log("Nombre de pages: "+nbrPageListe);
-    console.log("Rapidité du bot: 80 mails par minute");
-    console.log("Temps d'attente estimé: " + Math.round(waitingTime) + "min");
+    //navigation vers la dernière page pour connaitre le nombre total de pages
+    const fin = '#main > nav > span.last > a'; //selecteur css du lien vers la dernière page
+    await page.waitForSelector(fin);
+    await page.click(fin);
+    await page.waitForSelector('#main > nav > span.page.current');
+    const nbrPageListe = await page.$eval('#main > nav > span.page.current', el => el.innerText); //nombre de page
+    console.log("nombre de pages: "+nbrPageListe);
+    await page.goto(url);  //retour à la première page
 
-    const mailSelector = " div > div:nth-child(2) > a:nth-child(5)";
+    bar.start(nbrPageListe*nbrSiteListe, 0); //initialisation de la barre de progression
 
     for (var pg = 1; pg < nbrPageListe; pg++) { //selectionne chaque page
         for (var pasAgence = 0; pasAgence < nbrSiteListe; pasAgence++) {  //selectionne chaque agence 
@@ -27,7 +33,7 @@ const fs = require('fs');
             await clickSelector(Agences, page, pasAgence);
             try { //vérifie si la page existe bien
                 await page.waitForSelector(mailSelector, { timeout: 3000 })
-                if ((await (page.$eval(mailSelector, el => el.innerText))) != null) {  //récupère le texte dans le selecteur de mail si il est bien présent
+                if ((await (page.$eval(mailSelector, el => el.innerText.includes("@")))))  {  //récupère le texte dans le selecteur de mail si il est bien présent
                     const mail = await (page.$eval(mailSelector, el => el.innerText));
                     fs.writeFile('agences.txt', mail + "\n", { flag: 'a+' }, err => { //ecrit le mail dans le fichier agence.txt
                         if (err) {
@@ -39,8 +45,9 @@ const fs = require('fs');
             } catch (error) { }
             await page.goBack();
             await page.waitForSelector(Agences);
+            bar.increment();
         }
-        await page.goto(url + '&page=' + (pg + 1)); //navigue vers la page suivante
+        await page.goto(url + '?page=' + (pg + 1)); //navigue vers la page suivante
     }
     console.log('end');
     await browser.close();
@@ -48,7 +55,7 @@ const fs = require('fs');
 
 /**
  * Fonction permettant de cliquer sur le i-ème élément
- * @param {string} selector Selecteur css qui correspond *à l'élément à trouver et à cliquer
+ * @param {string} selector Selecteur css qui correspond à l'élément à trouver et à cliquer
  * @param {*} page Page web ouverte par Puppeteer
  * @param {number} i Selecteur css qui correspond aux éléments à trouver et à cliquer
  * 
